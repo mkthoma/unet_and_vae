72cd4f975dad2c3253cf4bbe96ca6d0c24aa386e
mkthoma <mathewkennythomas@gmail.com>
2023-09-21 18:50:48 +0530

    code push

--- UNET/train.py
+++ UNET/train.py
@@ -13,9 +13,9 @@ def unet_train(unet, X, y):
     # Split Train and Test Set
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=123)
 
-    # Modify the labels for U-Net
-    y_train = modify_labels_for_unet(y_train)
-    y_valid = modify_labels_for_unet(y_valid)
+    # # Modify the labels for U-Net
+    # y_train = modify_labels_for_unet(y_train)
+    # y_valid = modify_labels_for_unet(y_valid)
 
     # Run the model in a mini-batch fashion and compute the progress for each epoch
     results = unet.fit(X_train, y_train, batch_size=32, epochs=20, validation_data=(X_valid, y_valid))
